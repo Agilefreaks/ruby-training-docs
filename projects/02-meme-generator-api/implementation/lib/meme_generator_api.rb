6c9a976741c9a1598324d887243d13a0f3f7d553
Ana <anadanieladragan@gmail.com>
2022-04-08 11:32:09 +0300

    Implemented image modification

--- projects/02-meme-generator-api/implementation/lib/meme_generator_api.rb
+++ projects/02-meme-generator-api/implementation/lib/meme_generator_api.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
 require 'open-uri'
+require 'mini_magick'
+require 'sinatra'
+
+require './lib/post_image_with_sinatra'
 
 class MemeGeneratorAPI
-  def self.execute(uri_string)
+  def self.execute(uri_string, text)
+    # save image as file locally
     uri = URI.parse(uri_string)
     image_path = './images/image1.jpg'
     uri.open do |image|
@@ -11,5 +16,19 @@ class MemeGeneratorAPI
         file.write(image.read)
       end
     end
+
+    # add text to image
+    image = MiniMagick::Image.new('./images/image1.jpg')
+    image.combine_options do |c|
+      c.gravity 'center'
+      c.draw "text 0,0 \'#{text}\'"
+      c.undercolor 'White'
+      c.fill 'Black'
+      c.font 'Helvetica'
+      c.pointsize '60'
+    end
+
+    # post image
+    PostImageWithSinatra.run!
   end
 end
