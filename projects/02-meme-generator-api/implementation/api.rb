9671ab599ca5893da4ed3f50f41ffd9ff3e36bea
Ana <anadanieladragan@gmail.com>
2022-04-08 11:31:18 +0300

    Added parameters to execute method and removed todo comments

--- projects/02-meme-generator-api/implementation/api.rb
+++ projects/02-meme-generator-api/implementation/api.rb
@@ -4,19 +4,11 @@ require './lib/meme_generator_api'
 
 class API
   def self.run
-    uri_string = 'https://blog.theclymb.com/wp-content/uploads/2016/05/shutterstock_336348722-1-1024x683.jpg'
-    MemeGeneratorAPI.execute(uri_string)
+    #uri_string = 'https://blog.theclymb.com/wp-content/uploads/2016/05/shutterstock_336348722-1-1024x683.jpg'
+    uri_string = 'https://b-van.ro/wp-content/uploads/2020/12/Post1-Van-Life-5-beneficii-cover-v3.jpg'
+    text = 'Home is where you park it'
+    MemeGeneratorAPI.execute(uri_string, text)
   end
 end
 
 API.run
-
-# TODO:
-#   - save image file locally (get from url)
-#   - add text to image
-#   - redirect to image (in browser)
-
-# Work with:
-#   - Sinatra
-#   - MiniMagick
-#   - OpenURI
