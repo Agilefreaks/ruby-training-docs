b4e2ba55eecc2c3ac52e5faa2449fad4ce3b9e0d
Ana <anadanieladragan@gmail.com>
2022-04-09 14:48:41 +0300

    Renamed post and get methods

--- projects/02-meme-generator-api/implementation/app/controllers/application_controller.rb
+++ projects/02-meme-generator-api/implementation/app/controllers/application_controller.rb
@@ -18,16 +18,16 @@ class ApplicationController < Sinatra::Base
 
   enable :sessions
 
-  post '/create_meme' do
+  post '/meme' do
     @meme = Meme.new
     @meme.image_url = params[:image_url]
     @meme.text = params[:text]
     @meme.create
 
-    redirect "/display_meme/#{@meme.file_name}", 303
+    redirect "/meme/#{@meme.file_name}", 303
   end
 
-  get '/display_meme/:file_name' do
+  get '/meme/:file_name' do
     send_file(Meme.file_path(params[:file_name]))
   end
 end
--- projects/02-meme-generator-api/implementation/spec/app/controllers/application_controller_spec.rb
+++ projects/02-meme-generator-api/implementation/spec/app/controllers/application_controller_spec.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 require './app/controllers/application_controller'
-require 'sinatra'
-require 'rack/test'
 
 def app
   ApplicationController
@@ -20,15 +18,19 @@ describe 'GET /memes' do
   end
 end
 
-describe 'POST /create_meme' do
+describe 'POST /meme' do
   it 'redirects to created image' do
-    post '/create_meme', image_url:'https://images.unsplash.com/photo-1647549831144-09d4c521c1f1', text:'Start the way by organising your playground'
+    post '/meme', image_url:'https://images.unsplash.com/photo-1647549831144-09d4c521c1f1',
+                         text:'Start the way by organising your playground'
 
     expect(last_response.status).to eq(303)
+
+    # get
+    # check if image is the same as created file
   end
 end
 
-describe 'GET /display_meme' do
+describe 'GET /meme' do
   it 'opens the created image' do
   end
 end
