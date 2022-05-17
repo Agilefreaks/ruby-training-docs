5cdcc754c295f77a266e934b0e688cd9a2ca6c26
pirvudoru <pirvu.doru@ymail.com>
2022-05-17 16:16:19 +0300

    Improve example

--- projects/04-training-api/examples/nokogiri_example.rb
+++ projects/04-training-api/examples/nokogiri_example.rb
@@ -5,4 +5,13 @@ require 'nokogiri'
 require 'open-uri' # Load content from URL
 
 doc = Nokogiri::HTML(open('https://12ft.io/'))
-doc.xpath('//meta[@property="og:url"]').first['content']
+url = doc.xpath('//meta[@property="og:url"]').first['content']
+title = doc.xpath('//meta[@property="og:title"]').first['content']
+image = doc.xpath('//meta[@property="og:image"]').first['content']
+description = doc.xpath('//meta[@property="og:description"]').first['content']
+
+p "URL: #{url}"
+p "Title: #{title}"
+p "Image: #{image}"
+p "Description: #{description}"
+
